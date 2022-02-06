3adc2a77b2816966d05e807ad8fc97a6715241b9
Timothy <hsu_timothy@yahoo.com>
2022-02-05 22:42:06 -0600

    Render tiles for each object in props array

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -3,7 +3,9 @@ import React from "react";
 export const TileList = () => {
   return (
     <div>
-      
+      {props.contacts.forEach((contact) => {
+        return <Tile contact={contact} />
+      })}
     </div>
   );
 };
