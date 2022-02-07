fca588a88b01df7670e4489b690ad14ab88afff8
Timothy <hsu_timothy@yahoo.com>
2022-02-07 09:28:32 -0600

    Render data from props object

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 export const Tile = () => {
+  const values = Object.values(props.contact);
+  const tile = () => {
+    for (let i; i < values.length; i++) {
+      if (i === 0) {
+        <p className='tile-title' key={values[i]}>{props.contact[i]}</p>
+      } else {
+        <p className="tile" key={values[i]}>{[props.contact[i]]}</p>
+      }
+    }
+  }
+
   return (
     <div className="tile-container">
-      
+      {tile}
     </div>
   );
-};
+}
